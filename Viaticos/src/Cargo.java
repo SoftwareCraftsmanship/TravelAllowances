3959e89d2036ef70d47648b330767fef579faaf0
ecas007 <ecastro@mutualser.org>
2016-09-27 08:11:10 -0500

    cargo
    clase cargo

---
+++ Viaticos/src/Cargo.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ANA_SERVICIO5 on 24/09/2016.
+ */
+public class Cargo {
+    private int idCargo;
+    private String nombre;
+    private TipoCargo tipocargo;
+
+    public Cargo(int idCargo, String nombre, TipoCargo tipocargo) {
+        this.idCargo = idCargo;
+        this.nombre = nombre;
+        this.tipocargo = tipocargo;
+    }
+}
