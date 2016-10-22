9b2e449286e0b8c85d6e57d3265eb3f43e2e2580
ecas007 <ecastro@mutualser.org>
2016-10-22 09:38:22 -0500

    librerias

--- Viaticos/src/Cargo.java
+++ Viaticos/src/Cargo.java
@@ -12,4 +12,42 @@ public class Cargo{
         this.tipocargo = tipocargo;
     }
 
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public TipoCargo getTipocargo() {
+        return tipocargo;
+    }
+
+    public void setTipocargo(TipoCargo tipocargo) {
+        this.tipocargo = tipocargo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cargo cargo = (Cargo) o;
+
+        if (idCargo != cargo.idCargo) return false;
+        if (nombre != null ? !nombre.equals(cargo.nombre) : cargo.nombre != null) return false;
+        return tipocargo != null ? tipocargo.equals(cargo.tipocargo) : cargo.tipocargo == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idCargo;
+        result = 31 * result + (nombre != null ? nombre.hashCode() : 0);
+        result = 31 * result + (tipocargo != null ? tipocargo.hashCode() : 0);
+        return result;
+    }
+
+
 }
