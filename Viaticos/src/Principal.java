95d141cd24c08b3dee8d5b3f335ede8f2034afa1
Manuel Arrieta <manefran@outlook.com>
2016-10-26 11:28:08 -0500

    Modificacion clase Ubicacion y clase principal, se agrego listado de ubicaciones.

--- Viaticos/src/Principal.java
+++ Viaticos/src/Principal.java
@@ -1,9 +1,16 @@
 import Persist.GenericCrud;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by ANA_SERVICIO5 on 21/10/2016.
  */
 public class Principal {
+
+    private static List<Ubicacion> ubicacionList = new ArrayList<Ubicacion>();
+
     public static void main(String args[]){
 
         prueba();
@@ -21,5 +28,14 @@ public class Principal {
 
     private static void cargarDatos(){
 
+        ubicacionList.add(new Ubicacion("13001","Cartagena",true));
+        ubicacionList.add(new Ubicacion("08001","Barranquilla",true));
+        ubicacionList.add(new Ubicacion("47001","Barranquilla",true));
+
+        Iterator iter = ubicacionList.iterator();
+        while (iter.hasNext())
+            System.out.println(iter.next());
     }
+
 }
+
--- Viaticos/src/Ubicacion.java
+++ Viaticos/src/Ubicacion.java
@@ -1,25 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ANA_SERVICIO5 on 24/09/2016.
  */
 public class Ubicacion {
     private int idUbicacion;
+    private String codigoUbi;
     private String nobreUbi;
     private boolean pernocta;
 
-    public Ubicacion(int idUBicacion, String nobreUbi,  boolean pernocta){
-        this.idUbicacion = idUBicacion;
+
+    public Ubicacion(String codigoUbi,String nobreUbi, boolean pernocta){
+        this.codigoUbi = codigoUbi;
         this.nobreUbi = nobreUbi;
         this.pernocta = pernocta;
-
-
     }
 
     public void ActualizaUbi(Ubicacion u){
-
     }
 
     public void guardar(){
         System.out.println("Exito al guardar");
     }
 
+    @Override
+    public String toString() {
+        return "Ubicacion{" +
+                "codigoUbi='" + codigoUbi + '\'' +
+                ", nobreUbi='" + nobreUbi + '\'' +
+                '}';
+    }
 }
